8a8e6192b4319901dba11a153403239e530c9670
Lucas Alves <l.alves001@gmail.com>
2024-11-27 15:06:44 -0300

    connects to production database

--- infra/database.js
+++ infra/database.js
@@ -1,1 +1,40 @@
 import { Client } from "pg";
+
+function getClientConfig() {
+  return {
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  };
+}
+
+function getSSLValues() {
+  return process.env.NODE_ENV === "development" ? false : true;
+}
+
+/**
+ * Executes a SQL query on the PostgreSQL database.
+ * @param {string|{text: string, values: Array<any>}} queryObject - The SQL query to execute. Can be either a string representing the query or an object with `text` property representing the query text and `values` property representing the parameterized values.
+ * @returns {Promise<QueryResult<any>>} A Promise that resolves with the result of the SQL query.
+ */
+async function query(queryObject) {
+  const client = new Client(getClientConfig());
+
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
+}
+
+export default {
+  query: query,
+};
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,41 @@
-function status(request, response) {
-  response.status(200).json({});
+import database from "infra/database";
+
+async function status(request, response) {
+  const updatedAt = new Date().toISOString();
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: await getDatabaseVersion(),
+        max_connections: await getDatabaseMaxConnections(),
+        open_connections: await getDatabaseOpenConnections(),
+      },
+    },
+  });
+}
+
+async function getDatabaseVersion() {
+  const versionResult = await database.query("SHOW server_version;");
+
+  return versionResult.rows[0].server_version;
+}
+
+async function getDatabaseMaxConnections() {
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+
+  return parseInt(maxConnectionsResult.rows[0].max_connections);
+}
+
+async function getDatabaseOpenConnections() {
+  const databaseName = process.env.POSTGRES_DB;
+
+  const openConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  return openConnections.rows[0].count;
 }
 
 export default status;
