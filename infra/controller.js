f620b55f095be8403e599fad6ea7c566ba397090
Lucas Alves <l.alves001@gmail.com>
2025-05-06 11:44:57 -0300

    feat: create and use `ValidationError` in `controller.js`

--- infra/controller.js
+++ infra/controller.js
@@ -1,4 +1,8 @@
-import { InternalServerError, MethodNotAllowedError } from "@/infra/errors";
+import {
+  InternalServerError,
+  MethodNotAllowedError,
+  ValidationError,
+} from "@/infra/errors";
 
 function onNoMatchHandler(request, response) {
   const methodNotAllowed = new MethodNotAllowedError();
@@ -6,12 +10,15 @@ function onNoMatchHandler(request, response) {
 }
 
 function onErrorHandler(error, request, response) {
+  if (error instanceof ValidationError) {
+    return response.status(error.statusCode).json(error);
+  }
+
   const internalServerError = new InternalServerError({
     statusCode: error.statusCode,
     cause: error,
   });
 
-  console.log("\n Error on /api/v1/status controller:");
   console.error(internalServerError);
 
   response.status(internalServerError.statusCode).json(internalServerError);
--- infra/errors.js
+++ infra/errors.js
@@ -34,6 +34,24 @@ export class ServiceError extends Error {
   }
 }
 
+export class ValidationError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "A validation error has occurred.", { cause });
+    this.name = this.constructor.name;
+    this.action = action || "Please check the input data.";
+    this.statusCode = 400;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
 export class MethodNotAllowedError extends Error {
   constructor() {
     super("This method is not allowed for that endpoint.");
