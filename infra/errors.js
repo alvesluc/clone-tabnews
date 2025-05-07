d4e3beb435fa8e0f8e53376743c99fda6e30cbfc
Lucas Alves <l.alves001@gmail.com>
2025-05-07 11:40:14 -0300

    fix: fix the returned error name

--- infra/errors.js
+++ infra/errors.js
@@ -1,7 +1,7 @@
 export class InternalServerError extends Error {
   constructor({ cause, statusCode }) {
     super("An unexpected internal error occurred.", { cause });
-    this.name = this.constructor.name;
+    this.name = "InternalServerError";
     this.action = "Please contact support.";
     this.statusCode = statusCode || 500;
   }
@@ -19,7 +19,7 @@ export class InternalServerError extends Error {
 export class ServiceError extends Error {
   constructor({ cause, message }) {
     super(message || "Service currently unavailable.", { cause });
-    this.name = this.constructor.name;
+    this.name = "ServiceError";
     this.action = "Verify is the service is available.";
     this.statusCode = 503;
   }
@@ -37,7 +37,7 @@ export class ServiceError extends Error {
 export class ValidationError extends Error {
   constructor({ cause, message, action }) {
     super(message || "A validation error has occurred.", { cause });
-    this.name = this.constructor.name;
+    this.name = "ValidationError";
     this.action = action || "Please check the input data.";
     this.statusCode = 400;
   }
@@ -55,7 +55,7 @@ export class ValidationError extends Error {
 export class NotFoundError extends Error {
   constructor({ cause, message, action }) {
     super(message || "The requested resource was not found.", { cause });
-    this.name = this.constructor.name;
+    this.name = "NotFoundError";
     this.action = action || "Please check the resource identifier.";
     this.statusCode = 404;
   }
@@ -73,7 +73,7 @@ export class NotFoundError extends Error {
 export class MethodNotAllowedError extends Error {
   constructor() {
     super("This method is not allowed for that endpoint.");
-    this.name = this.constructor.name;
+    this.name = "MethodNotAllowedError";
     this.action = "Check if the HTTP request is valid for this endpoint.";
     this.statusCode = 405;
   }
